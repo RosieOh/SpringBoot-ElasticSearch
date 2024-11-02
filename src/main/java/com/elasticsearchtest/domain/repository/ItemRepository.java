412c3c35b2a3150f8ea87fa678fe304c9b13c044
RosieOh <20172207@gm.hannam.ac.kr>
2024-11-03 02:46:32 +0900

    ✨ FEAT : Item 비즈니스 로직 설명기반 검색 조건 및 최대/최소 가격 검색 조회조건 추가 및 ELS 커스텀 QueryDSL 추가

--- src/main/java/com/elasticsearchtest/domain/repository/ItemRepository.java
+++ src/main/java/com/elasticsearchtest/domain/repository/ItemRepository.java
@@ -1,7 +1,16 @@
 package com.elasticsearchtest.domain.repository;
 
 import com.elasticsearchtest.domain.model.Item;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends ElasticsearchRepository<Item, String>{
+    List<Item> findByName(String keyword);
+    List<Item> findByPriceBetween(int minPrice, int maxPrice);
+
+    // ElasticSearch 커스텀 쿼리
+    @Query("{\"match\": {\"description\": \"?0\"}}")
+    List<Item> searchByDescription(String description);
 }
